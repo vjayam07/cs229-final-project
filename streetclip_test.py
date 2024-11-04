03191079df0928dae221bf88dcba62d5d10d7b74
vjayam07 <virajjayam@gmail.com>
2024-11-03 17:43:53 -0800

    main fn

--- streetclip_test.py
+++ streetclip_test.py
@@ -7,7 +7,21 @@ Written by Viraj Jayam and Kapil Dheeriya, Stanford University, 3 Nov. 2024.
 import numpy as np
 import torch
 import torch.nn as nn
+import os
 
 import huggingface_hub
 import wandb
 
+from PIL import Image, ImageDraw, ImageFont
+import matplotlib.pyplot as plt
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__': 
+    main()
