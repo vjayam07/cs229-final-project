9dc9686d8d2bcef8bee61b5ff3698c41d1053690
vjayam07 <virajjayam@gmail.com>
2024-11-04 14:03:46 -0800

    some test additions

--- streetclip_test.py
+++ streetclip_test.py
@@ -8,20 +8,37 @@ import numpy as np
 import torch
 import torch.nn as nn
 import os
+import requests
 
-import huggingface_hub
+import huggingface_hub as hf
+from transformers import CLIPProcessor, CLIPModel
 import wandb
 
 from PIL import Image, ImageDraw, ImageFont
 import matplotlib.pyplot as plt
 
+def init_model():
+    model = CLIPModel.from_pretrained("geolocal/StreetCLIP")
+    processor = CLIPProcessor.from_pretrained("geolocal/StreetCLIP")
 
+    return model, processor
 
+def hf_test(model, processor):
+    url = "https://huggingface.co/geolocal/StreetCLIP/resolve/main/sanfrancisco.jpeg"
+    image = Image.open(requests.get(url, stream=True).raw)
 
+    choices = ["San Jose", "San Diego", "Los Angeles", "Las Vegas", "San Francisco"]
+    inputs = processor(text=choices, images=image, return_tensors="pt", padding=True)
+
+    outputs = model(**inputs)
+    logits_per_image = outputs.logits_per_image # this is the image-text similarity score
+    probs = logits_per_image.softmax(dim=1)
+
+    return probs
 
 def main():
-    pass
-
+    model, processor = init_model()
+    print(hf_test(model, processor))
 
 if __name__ == '__main__': 
     main()
