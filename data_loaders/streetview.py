9d2774fbb140049b008477899e5c62917033c213
vjayam07 <virajjayam@gmail.com>
2024-12-06 12:00:58 -0800

    train files

--- data_loaders/streetview.py
+++ data_loaders/streetview.py
@@ -10,14 +10,26 @@ from torch.utils.data import Dataset
 import clip
 
 class StreetViewDataset(Dataset):
-    def __init__(self, image_dir, list_txt):
-        self.image_dir = image_dir
-        self.title = clip.tokenize(list_txt)
+    '''
+    self.metadata: pandas df. 
+    self.processor: CLIP processor
+
+    Note the above initializations!
+    '''
+    def __init__(self, metadata, processor):
+        self.metadata = metadata
+        self.processor = processor
 
     def __len__(self):
-        return len(self.title)
+        return len(self.metadata)
 
     def __getitem__(self, idx):
-        img = Image.open(self.image_path[idx])
-        title = self.title[idx]
-        return img, title
+        image_path = self.metadata.iloc[idx]['filename']
+        image = Image.open(image_path).convert("RGB")
+        image = self.processor(images=image, return_tensors="pt").pixel_values[0]
+        
+        country = self.metadata.iloc[idx]['country']
+        country_text = f"A Street View photo in {country}."
+        country_text = self.processor(text=country_text, return_tensors="pt", padding=True, truncation=True).input_ids[0]
+
+        return image, country_text
